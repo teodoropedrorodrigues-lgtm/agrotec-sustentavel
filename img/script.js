24ebd89692a084048f59949bd9929922feafefcc
teodoropedrorodrigues-lgtm <teodoro.pedro.rodrigues@escola.pr.gov.br>
2026-06-10 14:36:45 -0300

    Add functions to display messages and quiz results

--- img/script.js
+++ img/script.js
@@ -1,1 +1,25 @@
 
+function mostrarMensagem() {
+
+    const tecnologias = [
+        "🚁 Drones monitoram plantações e reduzem desperdícios.",
+        "💧 Irrigação inteligente economiza água.",
+        "🛰️ Sensores ajudam no controle do solo.",
+        "🌱 Agricultura de precisão aumenta a produtividade."
+    ];
+
+    const sorteio =
+        tecnologias[Math.floor(Math.random() * tecnologias.length)];
+
+    document.getElementById("resultado").innerHTML = sorteio;
+}
+
+function respostaCorreta() {
+    document.getElementById("quizResultado").innerHTML =
+    "✅ Correto! O plantio direto ajuda a conservar o solo.";
+}
+
+function respostaErrada() {
+    document.getElementById("quizResultado").innerHTML =
+    "❌ Incorreto! O desmatamento prejudica o meio ambiente.";
+}
