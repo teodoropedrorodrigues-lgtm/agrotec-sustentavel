19a5c81111acd9e95162f597e19b25f72c87c38d
teodoropedrorodrigues-lgtm <teodoro.pedro.rodrigues@escola.pr.gov.br>
2026-06-12 14:24:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function calcular(){
 
+    let animais = Number(
+        document.getElementById("animais").value
+    );
+
+    if(animais <= 0 || isNaN(animais)){
+        document.getElementById("resultado").innerHTML =
+        "Informe uma quantidade válida.";
+        return;
+    }
+
+    let economia = animais * 15;
+
+    document.getElementById("resultado").innerHTML =
+    "Economia estimada: R$ " +
+    economia.toFixed(2) +
+    " por mês.";
+}
